e5c4282a73ac5bbadae972ab648c1495adc2dc6d
David Schlachter <david+gh@schlachter.ca>
2020-10-18 12:26:35 -0400

    Add overall spent, earned, and net change to email, improve styling

--- monthly-report.py
+++ monthly-report.py
@@ -5,9 +5,10 @@ import sys
 import traceback
 import datetime
 import requests
+import re
+import bs4
 import smtplib
 
-from bs4 import BeautifulSoup
 from email.message import EmailMessage
 from email.headerregistry import Address
 from email.utils import make_msgid
@@ -27,6 +28,7 @@ def main():
 	today = datetime.date.today()
 	endDate = today.replace(day=1) - datetime.timedelta(days=1)
 	startDate = endDate.replace(day=1)
+	monthName = startDate.strftime("%B")
 	#
 	# Set us up for API requests
 	HEADERS = {'Authorization': 'Bearer {}'.format(config['accesstoken'])}
@@ -53,29 +55,59 @@ def main():
 				categoryEarned = 0
 			categoryTotal  = float(categoryEarned) + float(categorySpent)
 			totals.append( {'name': categoryName, 'spent': categorySpent, 'earned': categoryEarned, 'total': categoryTotal} )
-		# Set up the email body
+		#
+		# Get general information
+		monthSummary = s.get(config['firefly-url'] + '/api/v1/summary/basic' + '?start=' + startDate.strftime('%Y-%m-%d') + '&end=' + endDate.strftime('%Y-%m-%d')).json()
+		yearToDateSummary = s.get(config['firefly-url'] + '/api/v1/summary/basic' + '?start=' + startDate.strftime('%Y') + '-01-01' + '&end=' + endDate.strftime('%Y-%m-%d')).json()
+		for key in monthSummary:
+			if re.match(r'spent-in-.*', key):
+				currencyName = key.replace("spent-in-", "")
+		spentThisMonth     = monthSummary['spent-in-'+currencyName]['monetary_value']
+		earnedThisMonth    = monthSummary['earned-in-'+currencyName]['monetary_value']
+		netChangeThisMonth = monthSummary['balance-in-'+currencyName]['monetary_value']
+		spentThisYear      = yearToDateSummary['spent-in-'+currencyName]['monetary_value']
+		earnedThisYear     = yearToDateSummary['earned-in-'+currencyName]['monetary_value']
+		netChangeThisYear  = yearToDateSummary['balance-in-'+currencyName]['monetary_value']
+		netWorth           = yearToDateSummary['net-worth-in-'+currencyName]['monetary_value']
+		#
+		# Set up the categories table
+		categoriesTableBody = '<table><tr><th>Category</th><th style="text-align: right;">Total</th></tr>'
+		#categoriesTableBody = '<table><tr><th>Category</th><th>Spent</th><th>Earned</th><th>Total</th></tr>'
+		for category in totals:
+			categoriesTableBody += '<tr><td style="padding-right: 1em;">'+category['name']+'</td><td style="text-align: right;">'+str(round(float(category['total']))).replace("-", "−")+'</td></tr>'
+			#categoriesTableBody += '<tr><td>'+category['name']+'</td><td>'+str(round(float(category['spent'])))+'</td><td>'+str(round(float(category['earned'])))+'</td><td>'+str(round(float(category['total'])))+'</td></tr>'
+		categoriesTableBody += '</table>'
+		#
+		# Set up the general information table
+		generalTableBody = '<table>'
+		generalTableBody += '<tr><td>Spent this month:</td><td style="text-align: right;">' + str(round(spentThisMonth)).replace("-", "−") + '</td></tr>'
+		generalTableBody += '<tr><td>Earned this month:</td><td style="text-align: right;">' + str(round(earnedThisMonth)).replace("-", "−") + '</td></tr>'
+		generalTableBody += '<tr style="border-bottom: 1px solid black"><td>Net change this month:</td><td style="text-align: right;">' + str(round(netChangeThisMonth)).replace("-", "−") + '</td></tr>'
+		generalTableBody += '<tr><td>Spent so far this year:</td><td style="text-align: right;">' + str(round(spentThisYear)).replace("-", "−") + '</td></tr>'
+		generalTableBody += '<tr><td>Earned so far this year:</td><td style="text-align: right;">' + str(round(earnedThisYear)).replace("-", "−") + '</td></tr>'
+		generalTableBody += '<tr style="border-bottom: 1px solid black"><td style="padding-right: 1em;">Net change so far this year:</td><td style="text-align: right;">' + str(round(netChangeThisYear)).replace("-", "−") + '</td></tr>'
+		generalTableBody += '<tr><td>Current net worth:</td><td style="text-align: right;">' + str(round(netWorth)).replace("-", "−") + '</td></tr>'
+		generalTableBody +='</table>'
+		#
+		# Assemble the email
 		msg = EmailMessage()
-		msg['Subject'] = "Firefly III: Monthly categories report"
+		msg['Subject'] = "Firefly III: Monthly report"
 		msg['From'] = "monthly-report <" + config['email']['from'] + ">"
 		msg['To'] = ( tuple(config['email']['to']) )
-		#tableBody = '<table><tr><th>Category</th><th>Spent</th><th>Earned</th><th>Total</th></tr>'
-		tableBody = '<table><tr><th>Category</th><th>Total</th></tr>'
-		for category in totals:
-			#tableBody += '<tr><td>'+category['name']+'</td><td>'+str(round(float(category['spent'])))+'</td><td>'+str(round(float(category['earned'])))+'</td><td>'+str(round(float(category['total'])))+'</td></tr>'
-			tableBody += '<tr><td>'+category['name']+'</td><td>'+str(round(float(category['total']))).replace("-", "−")+'</td></tr>'
-		tableBody += '</table>'
 		htmlBody = """
 		<html>
 			<head>
-				<style>table{{border-collapse: collapse; border-top: 1px solid black; border-bottom: 1px solid black;}} th {{border-bottom: 1px solid black; padding: 0.33em 1em 0.33em 1em;}} td{{padding: .1em;}} tr:nth-child(even) {{background: #EEE}} tr:nth-child(odd) {{background: #FFF}} tr td:first-child {{padding-right: 1em;}} tr td:last-child, tr th:last-child {{text-align: right;}}</style>
+				<style>table{{border-collapse: collapse; border-top: 1px solid black; border-bottom: 1px solid black;}} th {{border-bottom: 1px solid black; padding: 0.33em 1em 0.33em 1em;}} td{{padding: .1em;}} tr:nth-child(even) {{background: #EEE}} tr:nth-child(odd) {{background: #FFF}}</style>
 			</head>
 			<body>
-				<p>Monthly categories report for {start} to {end}:</p>
-				{tableBody}
+				<p>Monthly report for {monthName} {year}:</p>
+				{categoriesTableBody}
+				<p>General information:</p>
+				{generalTableBody}
 			</body>
 		</html>
-		""".format( start=startDate.strftime('%Y-%m-%d'), end=endDate.strftime('%Y-%m-%d'), tableBody=tableBody )
-		msg.set_content(BeautifulSoup(htmlBody).get_text()) # just html to text
+		""".format( monthName=monthName, year=startDate.strftime("%Y"), categoriesTableBody=categoriesTableBody, generalTableBody=generalTableBody )
+		msg.set_content(bs4.BeautifulSoup(htmlBody, "html.parser").get_text()) # just html to text
 		msg.add_alternative(htmlBody, subtype='html')
 		#
 		# Send off the message
